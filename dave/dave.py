92ef3d5ccda4cea285c745da2800ad6667ae8c42
zack5 <zack5@users.noreply.github.com>
2017-07-03 14:16:24 -0700

    Started estimated completion date
    Hijacks Estimated Time Remaining slot to display estimated completion date instead

--- dave/dave.py
+++ dave/dave.py
@@ -627,7 +627,7 @@ class Dave(QtGui.QMainWindow):
             self.running = False
 
         # Start
-        else: 
+        else:
 
             # Confirm run in the presence of invalid commands
             if not self.ui.commandSequenceTreeView.isAllValid():
@@ -795,9 +795,10 @@ class Dave(QtGui.QMainWindow):
     @hdebug.debug
     def updateEstimates(self):
         [est_time, est_space] = self.ui.commandSequenceTreeView.getEstimates()
-            
         self.ui.timeLabel.setText("Run Duration: " + str(datetime.timedelta(seconds=est_time))[0:8])
-        self.ui.remainingLabel.setText("Time Remaining: " + str(datetime.timedelta(seconds=est_time))[0:8])
+        #self.ui.remainingLabel.setText("Time Remaining: " + str(datetime.timedelta(seconds=est_time))[0:8])
+        endTime = datetime.timedelta(seconds=est_time) + datetime.datetime.now()
+        self.ui.remainingLabel.setText("Expected Completion: " + endTime.strftime("%I:%M%p %B %d, %Y"))
         if est_space/2**10 < 1.0: # Less than GB
             self.ui.spaceLabel.setText("Run Size: {0:.2f} MB ".format(est_space))
         elif est_space/2**20 < 1.0: # Less than TB
